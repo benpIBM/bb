179eb4411ae81b72510c8aabf7dc0fb2a485b744
benpIBM <perlmutt@us.ibm.com>
2015-05-27 13:59:41 -0700

    onsubmit calls function with value of PID form

--- public/script.js
+++ public/script.js
@@ -1,5 +1,10 @@
 function lookupPID(){
 	
-	alert("hello world!!!");
+    var pid = document.getElementById("PID_form");
+
+    if (pid.elements[0]) {
+    	alert(pid.elements[0].value);
+    }
 
 }
+
